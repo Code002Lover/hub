be5a1a2c4f9db4e5f821f1681dba6911b1bc647a
Mystikfluu <50167590+Mystikfluu@users.noreply.github.com>
2021-02-12 17:37:14 +0100

    Delete HouseTycoon.lua

--- HouseTycoon.lua
+++
@@ -1,6 +0,0 @@
-while getgenv().yes do
-    if(getgenv().Key == "T8VhdrryJULwsptlGTYKWqNSxjWqg6jGuWfUCYldjfIbdCnY9kB9OufuEvrjzclOLIEeuPfUA7TTsx6pldBLVnUSdfUebR53ua4K") then
-        game:GetService("ReplicatedStorage").Events.Remote.CollectCash:FireServer()
-        wait()
-    end
-end
